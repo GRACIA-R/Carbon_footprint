44d5669f593a48882cdf49654e280a20a3033000
Young_ChemEng <chemicalrola@gmail.com>
2026-04-13 20:55:31 -0700

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,26 @@
+import gspread
+from google.oauth2.service_account import Credentials
+import streamlit as st
 import pandas as pd
 import os
 
-FILE = "results/responses.csv"
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive",
+]
+SHEET_NAME = "carbon_footprint_responses"  # nombre de tu Google Sheet
+WORKSHEET = "Sheet1"
+
+def _get_sheet():
+    creds_dict = dict(st.secrets["gcp_service_account"])
+    creds = Credentials.from_service_account_info(creds_dict, scopes=SCOPES)
+    client = gspread.authorize(creds)
+    sheet = client.open(SHEET_NAME).worksheet(WORKSHEET)
+    return sheet
 
 def save_response(data):
-
-    df = pd.DataFrame([data])
-
-    if os.path.exists(FILE):
-        df_old = pd.read_csv(FILE)
-        df = pd.concat([df_old, df], ignore_index=True)
-
-    df.to_csv(FILE, index=False)
+    sheet = _get_sheet()
+    # Si la hoja está vacía, agrega encabezados primero
+    if sheet.row_count == 0 or sheet.cell(1, 1).value is None:
+        sheet.append_row(list(data.keys()))
+    sheet.append_row(list(data.values()))
