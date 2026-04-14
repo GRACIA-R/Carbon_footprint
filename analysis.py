98023068b984c31b69e606fc2d1d4719913fa91d
Young_ChemEng <chemicalrola@gmail.com>
2026-04-13 20:55:46 -0700

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,11 +1,22 @@
+import gspread
+from google.oauth2.service_account import Credentials
+import streamlit as st
 import pandas as pd
 
-FILE = "results/responses.csv"
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive",
+]
+SHEET_NAME = "carbon_footprint_responses"
+WORKSHEET = "Sheet1"
 
 def load_data():
-
     try:
-        df = pd.read_csv(FILE)
-        return df
-    except:
+        creds_dict = dict(st.secrets["gcp_service_account"])
+        creds = Credentials.from_service_account_info(creds_dict, scopes=SCOPES)
+        client = gspread.authorize(creds)
+        sheet = client.open(SHEET_NAME).worksheet(WORKSHEET)
+        data = sheet.get_all_records()
+        return pd.DataFrame(data)
+    except Exception as e:
         return pd.DataFrame()
